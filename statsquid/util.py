f2e158bbd2da03ebb5523ddb8a548c306b0214e5
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-04-19 21:12:11 -0400

    add spacing to format_bytes

--- statsquid/util.py
+++ statsquid/util.py
@@ -1,12 +1,12 @@
 def format_bytes(b):
     b = float(b)
     if b < 1000:
-        return '%i' % b + 'b'
+        return '%i' % b + ' b'
     elif 1000 <= b < 1000000:
-        return '%.1f' % float(b/1000) + 'kb'
+        return '%.1f' % float(b/1000) + ' kb'
     elif 1000000 <= b < 1000000000:
-        return '%.1f' % float(b/1000000) + 'mb'
+        return '%.1f' % float(b/1000000) + ' mb'
     elif 1000000000 <= b < 1000000000000:
-        return '%.1f' % float(b/1000000000) + 'gb'
+        return '%.1f' % float(b/1000000000) + ' gb'
     elif 1000000000000 <= b:
-        return '%.1f' % float(b/1000000000000) + 'tb'
+        return '%.1f' % float(b/1000000000000) + ' tb'
