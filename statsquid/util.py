9f972d08ebf9e07ddbe835473cc4def0ee30426e
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-04-27 17:15:01 -0400

    refactor container,top
    move delta methods for net and io stats into top utility
    store only cummulative stats in redis for new and io
    add cummulative and current display modes to top
    add maintenance interval to listener to regularly remove unreporting containers

--- statsquid/util.py
+++ statsquid/util.py
@@ -18,6 +18,14 @@ def unix_time(dt):
     delta = dt - epoch
     return int(round(delta.total_seconds()))
 
+def convert_type(value,to_type):
+    if to_type == str:
+        return str(value)
+    if to_type == float:
+        return float(value)
+    if to_type == int:
+        return int(value)
+
 def output(msg):
     #wrapper to append date to printed message
     print('%s: %s' % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), msg))
